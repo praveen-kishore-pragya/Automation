1b7ecb77bfbace0ac1a33f74bf154df429a974f0
Praveen Kishore Pragya <72307064+praveen-kishore-pragya@users.noreply.github.com>
2023-02-10 13:43:51 +0530

    Add files via upload

--- Automation Python/dell_com.py
+++ Automation Python/dell_com.py
@@ -64,6 +64,8 @@ time.sleep(1)
 #press on search button,or enable press enter on previous step
 # driver.find_element(By.XPATH,'//*[@id="unified-masthead"]/div[1]/div[1]/div[2]/button[2]/svg').click()  --- NOT ABLE TO DO
 
+#filter the laptops --> precaution so NO other type of items show up
+driver.find_element(By.XPATH,'//*[@id="refinement-36679"]').click()
 
 # select the product
 driver.find_element(By.XPATH,'//*[@id="d569946win9"]/section[1]/h3/a').click()
@@ -71,17 +73,19 @@ time.sleep(1)
 
 # buy the product
 driver.find_element(By.XPATH,'//*[@id="add-to-cart-stack"]/div[2]/a').click()
+# driver.set_page_load_timeout(20)
 time.sleep(20)
 
-# enter pin code
-driver.find_element(By.NAME,"zipcode").send_keys("560048")
-time.sleep(1)
+#checking availability
+# # enter pin code
+# driver.find_element(By.XPATH,'//*[@id="zipcode"]').send_keys("560048")
+# time.sleep(1)
 
-# click check button
-driver.find_element(By.ID,"button-zipcode-check2").click()
-time.sleep(1)
+# # click check button
+# driver.find_element(By.XPATH,'//*[@id="button-zipcode-check2"]').click()
+# time.sleep(1)
 
-res = driver.find_element(By.XPATH,'//*[@id="form-product-zipcode2"]/div/div[3]')
+# res = driver.find_element(By.XPATH,'//*[@id="form-product-zipcode2"]/div/div[3]')
 # check if product can  be delivered to the pin code mentioned and if yes buy the product
 # if isSubsequence("Delivery", res)==True:
 #     driver.find_element(By.ID,"bundle-slide").click()
@@ -89,7 +93,46 @@ res = driver.find_element(By.XPATH,'//*[@id="form-product-zipcode2"]/div/div[3]'
 # else:
 #     print("Product is currently not deliverable")
 
+#click buy now to go to checkout
+driver.find_element(By.XPATH,'//*[@id="bundle-slide"]').click()
+time.sleep(1)
 
+#enter details before pay now
+#enter email
+driver.find_element(By.XPATH,'//*[@id="customer-email"]').send_keys(email)
+
+#enter personal details
+firstName = "kojapov"
+lastName = "breazeim"
+phoneNo = "1234567890"
+StreetAdd = "Mahadevapura"
+pinCode = "560048"
+
+driver.find_element(By.XPATH,'//*[@id="RRG9GMF"]').send_keys(firstName)
+time.sleep(1)
+driver.find_element(By.XPATH,'//*[@id="XJ7CALD"]').send_keys(lastName)
+time.sleep(1)
+driver.find_element(By.XPATH,'//*[@id="GTPIYVV"]').send_keys(phoneNo)
+time.sleep(1)
+driver.find_element(By.XPATH,'//*[@id="E7DG1M1"]').send_keys(StreetAdd)
+time.sleep(1)
+driver.find_element(By.XPATH,'//*[@id="PKACLMM"]').send_keys(pinCode)
+time.sleep(1)
+
+#check box of AGREE terms
+driver.find_element(By.XPATH,'//*[@id="agreement__1"]').click()
+time.sleep(1)
+
+#enter pan number
+panNumber = "ABCDE1234F"
+driver.find_element(By.XPATH,'//*[@id="opc-sidebar"]/div[2]/div[2]/fieldset[2]/div/input').send_keys(panNumber)
+time.sleep(1)
+
+#checkout by clicking on PAY NOW
+driver.find_element(By.XPATH,'//*[@id="opc-sidebar"]/div[2]/div[3]/div/div/button').click()
+time.sleep(1)
+
+print("Test successful!!")
 
 
 driver.close()
